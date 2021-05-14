724bb7983ea165d1ef2c5c253a04a341e68cba0e
Jingru Wang <jingru@synopsys.com>
2021-05-14 10:45:47 +0800

    builder: modify build module
    use threading in subprocess
    use platform class to get board info
    get size by reading elf
    
    Signed-off-by: Jingru Wang <jingru@synopsys.com>

--- embarc_tools/embarc_subcommands/build.py
+++ embarc_tools/embarc_subcommands/build.py
@@ -1,123 +1,69 @@
 from __future__ import print_function, division, unicode_literals
 import os
 import sys
-from ..project import Generator
-from ..settings import get_input, get_config
+from ..settings import get_input
 from ..builder import build
-from ..utils import cd, getcwd, read_json
+from ..utils import getcwd
+
+
 help = "Build application"
 description = ("Compile code using toolchain.\n"
                "Currently supported Toolchain: GNU, MetaWare.")
 
 
 def run(args, remainder=None):
-    osproot = None
-    app_path = args.path
-    recordBuildConfig = dict()
-    app_path = os.path.abspath(app_path)
+    cached_config = args.config
+    if not cached_config:
+        if os.path.exists(os.path.join(args.outdir, "build.json")):
+            cached_config = os.path.join(args.outdir, "build.json")
+        elif os.path.exists(os.path.join(args.directory, "build.json")):
+            cached_config = os.path.join(args.directory, "build.json")
 
-    if not (os.path.exists(app_path) and os.path.isdir(app_path)):
-        print("[embARC] This is not a valid application path")
-        return
-
-    embarc_config = args.app_config
-
-    if not (embarc_config and os.path.exists(embarc_config)):
-        embarc_config = os.path.join(app_path, "embarc_app.json")
-
-    if os.path.exists(embarc_config):
-        print("[embARC] Read embarc_app.json")
-        recordBuildConfig = read_json(embarc_config)
-
-    parallel = args.parallel
-    if args.board:
-        recordBuildConfig["BOARD"] = args.board
-    if args.bd_ver:
-        recordBuildConfig["BD_VER"] = args.bd_ver
-    if args.core:
-        recordBuildConfig["CUR_CORE"] = args.core
-    if args.toolchain:
-        recordBuildConfig["TOOLCHAIN"] = args.toolchain
-    if args.olevel:
-        recordBuildConfig["OLEVEL"] = args.olevel
-    if remainder:
-        if (remainder[0]).startswith("-"):
-            print("embarc build: error: invalid parameter %s" % remainder[0])
-            sys.exit(1)
-        make_config, target = get_config(remainder)
-        if target:
-            args.target = target
-        recordBuildConfig.update(make_config)
-
-    builder = build.embARC_Builder(osproot, recordBuildConfig, args.outdir)
+    builder = build.embARC_Builder(
+        source_dir=args.directory, build_dir=args.outdir,
+        board=args.board, board_version=args.bd_ver,
+        core=args.core, toolchain=args.toolchain,
+        buildopts=args.build_opt,
+        embarc_config=cached_config
+    )
     if args.export:
-        builder.get_build_cmd(app_path, target=None, parallel=parallel, silent=False)
-        with cd(app_path):
-            if os.path.exists(".project") and os.path.exists(".cproject"):
-                while True:
-                    yes = get_input("The IDE project already exists, recreate and overwrite the old files [Y/N]  ")
-                    if yes in ["yes", "Y", "y"]:
-                        break
-                    elif yes in ["no", "n", "N"]:
-                        return
-                    else:
-                        continue
-            generator = Generator()
-            recordBuildConfig = read_json(embarc_config)
-            for project in generator.generate(buildopts=recordBuildConfig):
-                project.generate()
+        if os.path.exists(os.path.join(builder.build_dir, ".project")) and os.path.exists(os.path.join(builder.build_dir, ".cproject")):
+            while True:
+                yes = get_input("The IDE project already exists, recreate and overwrite the old files [Y/N]  ")
+                if yes in ["yes", "Y", "y"]:
+                    break
+                elif yes in ["no", "n", "N"]:
+                    return
+                else:
+                    continue
+        builder.generate_ide()
         sys.exit(0)
-    if args.target:
-        information = None
-        if args.target == "elf":
-            information = builder.build_elf(app_path, parallel=parallel, pre_clean=False, post_clean=False)
-        elif args.target == "bin":
-            information = builder.build_bin(app_path, parallel=parallel, pre_clean=False, post_clean=False)
-        elif args.target == "hex":
-            information = builder.build_hex(app_path, parallel=parallel, pre_clean=False, post_clean=False)
-        elif args.target == "clean":
-            information = builder.clean(app_path, parallel=parallel)
-        elif args.target == "distclean":
-            information = builder.distclean(app_path, parallel=parallel)
-        elif args.target == "boardclean":
-            information = builder.boardclean(app_path, parallel=parallel)
-        elif args.target == "info":
-            information = builder.get_build_info(app_path, parallel=parallel)
-        elif args.target == "size":
-            information = builder.get_build_size(app_path, parallel=parallel)
-        elif args.target:
-            information = builder.build_target(app_path, target=args.target, parallel=parallel, coverity=False)
-        else:
-            print("[embARC] Please choose right target")
-        if information:
-            if information.get("result") is False:
-                print("[embARC] Failed: {}".format(information.get("reason")))
+    builder.build_target(args.target)
 
 
 def setup(subparsers):
     subparser = subparsers.add_parser('build', help=help, description=description)
     subparser.add_argument(
-        "-d", "--path", default=getcwd(), help="application path", metavar='')
+        "-d", "--directory", default=getcwd(), help="application path", metavar='')
     subparser.add_argument(
-        "--outdir", help="output objs root directory", metavar='')
+        "-O", "--outdir", default=os.path.join(os.getcwd(), "build-out"),
+        help="Output directory for logs and binaries. ", metavar='')
     subparser.add_argument(
         "-b", "--board", help="choose board", metavar='')
     subparser.add_argument(
-        "--bd_ver", help="choose board version", metavar='')
+        "--bd-ver", help="choose board version", metavar='')
     subparser.add_argument(
         "--core", help="choose core", metavar='')
     subparser.add_argument(
-        "-o", "--olevel", default="O3", choices=["Os", "O0", "O1", "O2", "O3"], help="set olevel", metavar='')
-    subparser.add_argument(
         "--toolchain", choices=["mw", "gnu"], help="choose toolchain", metavar='')
-    subparser.add_argument(
-        "-j", "--parallel", type=int, help="build application with -j", metavar='')
     subparser.add_argument(
         "--target", default="all",
         help="choose build target, default target is all", metavar='')
     subparser.add_argument(
         "-g", "--export", action="store_true", help="generate IDE project files for your application")
     subparser.add_argument(
-        "--app_config", help="specify application configuration, default is to look for embarc_app.json", metavar='')
+        "--config", help="Specify application configuration, default is to look for build.json", metavar='')
+    subparser.add_argument('-o', '--build-opt', default=[], action='append',
+                           help='''options to pass to the build tool make
+                           may be given more than once''')
     subparser.set_defaults(func=run)
-
